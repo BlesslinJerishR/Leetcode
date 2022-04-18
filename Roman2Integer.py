5d38f4efe7ef82de66619c57b90047fca3679c0b
BlesslinJerishR <mastermindx33@gmail.com>
2022-04-18 13:28:41 +0530

    One @ Roman2Integer

---
+++ Roman2Integer.py
@@ -0,0 +1,21 @@
+# Roman to Integer
+# I V X  L  C  	D	M
+# 1 5 10 50 100 500 1000
+
+ri = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+}
+
+roman = str(input("Enter a Roman Value : ")).upper()
+value = []
+for rome in list(roman):
+    num = ri.get(rome)
+    value.append(num)
+
+print(sum(value))
